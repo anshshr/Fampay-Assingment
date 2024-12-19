2adce910d90a1726b6d37891d07ea9108d9384e9
anshshr <anshshrivastav24@gmail.com>
2024-12-19 23:22:26 +0530

    removed the unnecessry folder and some  commnets

--- fam_assingment/lib/pages/cards/HC3_card.dart
+++ fam_assingment/lib/pages/cards/HC3_card.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:fam_assingment/utils/color_convertor.dart'; // Update as per your utils path.
+import 'package:fam_assingment/utils/color_convertor.dart'; 
 
 class Hc3Card extends StatefulWidget {
   final dynamic data;
